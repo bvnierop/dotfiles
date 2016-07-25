d9cbdaccba9e7123ee050abed2defd5ea62c1a0f
Bart van Nierop <b.vannierop@gmail.com>
2016-07-25 22:10:39 +0200

    fix installer for windows

--- install.py
+++ install.py
@@ -42,11 +42,11 @@ os_symlink = getattr(os, 'symlink', None)
 if not callable(os_symlink):
     def symlink_ms(source, link_name):
         import ctypes
-        csl = ctypes.windll.kernel32.CreateSymbolicLinkW
-        csl.argtypes = (ctypes.c_wchar_p, ctypes.c_char_p, ctypes.c_uint32)
+        csl = ctypes.windll.kernel32.CreateSymbolicLinkA
+        csl.argtypes = (ctypes.c_char_p, ctypes.c_char_p, ctypes.c_uint32)
         csl.restype = ctypes.c_ubyte
         flags = 1 if os.path.isdir(source) else 0
-        if csl(link_name, source.replace('/', '\\'), flags) == 0:
+        if csl(link_name, source, flags) == 0:
             raise ctypes.WinError()
     os.symlink = symlink_ms
 
