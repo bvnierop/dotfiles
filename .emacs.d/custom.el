84f25278cbf732074af69914e71ad6abe3e8883e
Bart van Nierop <bart@mutablevoid.org>
2019-07-04 09:16:26 +0200

    Emacs: Updated custom.el

--- .emacs.d/custom.el
+++ .emacs.d/custom.el
@@ -7,6 +7,7 @@
    (quote
     ("42b9d85321f5a152a6aef0cc8173e701f572175d6711361955ecfb4943fe93af" "7366916327c60fdf17b53b4ac7f565866c38e1b4a27345fe7facbf16b7a4e9e8" "b050365105e429cb517d98f9a267d30c89336e36b109a1723d95bc0f7ce8c11d" default)))
  '(initial-frame-alist (quote ((fullscreen . maximized))))
+ '(org-agenda-files nil)
  '(package-selected-packages
    (quote
     (company-terraform terraform-mode hcl-mode dockerfile-mode tuareg utop ocp-indent merlin slime-company paredit slime slim-mode rspec-mode enh-ruby-mode company-anaconda anaconda-mode company flycheck popwin helm-ag robe magit markdown-mode dtrt-indent use-package helm-projectile gruvbox-theme free-keys evil))))
