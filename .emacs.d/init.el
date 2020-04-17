8589ba9b7dca8a15b135e9289482ae9ec5e0d509
Bart van Nierop <bart@mutablevoid.org>
2020-04-17 08:54:29 +0200

    Emacs: Support multiple init files
    To develop a new Emacs config, it helps if we can use multiple ways to
    initialize. This commits supports that by reading the config file from
    an environment variable. Defaults to `config.org`.

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -1,3 +1,2 @@
-;;;(package-initialize)
-
-(org-babel-load-file (expand-file-name "config.org" user-emacs-directory))
+(let ((org-mode-config-file (or (getenv "EMACS_CONFIG_ORG_FILE") "config.org")))
+  (org-babel-load-file (expand-file-name org-mode-config-file user-emacs-directory)))
